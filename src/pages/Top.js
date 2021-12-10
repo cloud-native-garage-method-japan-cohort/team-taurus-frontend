0d4127d8f2f75a0ec2e3cd9efb404bcec2e79066
Kohei Obe <kohei.obe@ibm.com>
2021-12-10 12:04:25 +0900

    modify placeholder

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -57,7 +57,7 @@ const Top = () => {
         <Paper className={classes.root}>
           <InputBase
             className={classes.input}
-            placeholder="Watson Discovery で検索"
+            placeholder="ex: チョコレート"
             inputProps={{ 'aria-label': 'search watson discovery' }}
             onChange={(e)=>{setSendText(e.target.value)}}
           />
